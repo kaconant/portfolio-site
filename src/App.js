d819a72a1ef72c40db2f4ae6ac7160da7060090e
kaconant <krissyconant@gmail.com>
2018-08-14 18:33:17 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -13,22 +13,20 @@ class App extends Component {
               <p className="about">FULL STACK DEVELOPER</p>
               <p className="about">DIGITAL MARKETER</p>
           </div>
-        </div>
-        <div className="container">
           <div className="row">
-            <div className="row projects">
-              <div className="column proj1">
-                <div className="proj p1"> <img src="/img/myBlend.png" className="projImg"/></div>
-              </div>
-              <div className="column proj2">
-                <div className="proj p2"> <img src="/img/memoryGame.png" className="projImg"/></div>
-              </div>
-              <div className="column proj3">
-                <div className="proj p3"> <img src="/img/sceneIt.png" className="projImg"/></div>
-              </div>
-              <div className="column proj4">
-                <div className="proj p4"> <img src="/img/basicCoffeeApp.png" className="projImg"/></div>
-              </div>
+            <div className="col-6 proj1">
+              <div className="proj p1"> <img src="/img/myBlend.png" className="projImg"/></div>
+            </div>
+            <div className="col-6 proj2">
+              <div className="proj p2"> <img src="/img/memoryGame.png" className="projImg"/></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6 proj3">
+              <div className="proj p3"> <img src="/img/sceneIt.png" className="projImg"/></div>
+            </div>
+            <div className="col-6 proj4">
+              <div className="proj p4"> <img src="/img/basicCoffeeApp.png" className="projImg"/></div>
             </div>
           </div>
         </div>
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footerContainer">
+
+      </div>
+    );
+  }
+}
+
+export default Footer;
