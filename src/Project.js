a75e24e291d95f8dd291b8368ae6fff5a0008427
kaconant <krissyconant@gmail.com>
2018-09-01 16:29:19 -0400

    updated projects page

--- src/Project.js
+++ src/Project.js
@@ -67,6 +67,7 @@ class Project extends Component {
           <div className='row'>
             <div onMouseEnter={this.mouseEnter1} onMouseLeave={this.mouseLeave1} className='card proj'>
               <h3 className='projTitle'>myBlendology</h3>
+              <h4 className='projDesc'>myBlendology is a web application comprised of a PERN stack (Postgres, Express, React, and Node.js) that allows you to create diffuser essential oil blends based off of a user's choice of mood.</h4>
               <img className='card-img projImg' src='/img/myBlendology.png' alt='myBlendology' />
               <div className='card-img-overlay'>
                 {this.state.isMouseInsideProj1 ? <a className='projBtn' href='http://www.myblendology.com/' target="_blank" rel="noopener noreferrer">Website ➜</a> : null}
@@ -75,6 +76,7 @@ class Project extends Component {
             </div>
             <div onMouseEnter={this.mouseEnter2} onMouseLeave={this.mouseLeave2} className='card proj'>
               <h3 className='projTitle'>CoffeeStarter</h3>
+              <h4 className='projDesc'>Coffee Run is a basic application that uses HTML / CSS / JavaScript / jQuery to produce coffee orders. This project was to focus on using jQuery to build forms and produce results as well as manipulate local storage.</h4>
               <img className='card-img projImg' src='/img/coffeeApp.png' alt='coffeeApp' />
               <div className='card-img-overlay'>
                 {this.state.isMouseInsideProj2 ? <a className='projBtn' href='https://optimistic-tesla-3dca05.netlify.com/' target="_blank" rel="noopener noreferrer">Website ➜</a> : null}
@@ -83,6 +85,7 @@ class Project extends Component {
             </div>
             <div onMouseEnter={this.mouseEnter3} onMouseLeave={this.mouseLeave3} className='card proj'>
               <h3 className='projTitle'>Memory Game</h3>
+              <h4 className='projDesc'>This Memory Game was created with the Create-React-App and was an exercise to use logic to create a matching card game as well as practice routing, switching, and linking.</h4>
               <img className='card-img projImg' src='/img/memoryGame.png' alt='memoryGame' />
               <div className='card-img-overlay'>
                 {this.state.isMouseInsideProj3 ? <a className='projBtn' href='https://vigilant-fermi-f504bc.netlify.com/' target="_blank" rel="noopener noreferrer">Website ➜</a> : null}
@@ -91,6 +94,7 @@ class Project extends Component {
             </div>
             <div onMouseEnter={this.mouseEnter4} onMouseLeave={this.mouseLeave4} className='card proj'>
               <h3 className='projTitle'>Slopey Seconds</h3>
+              <h4 className='projDesc'>Slopey Seconds combines the Google Maps API, Powder Project API, and Open Weather API to search for specific ski / snowboarding trail names and allow users to review their public rating, trail length, elevation, and weather.</h4>
               <img className='card-img projImg' src='/img/slopeySeconds.png' alt='slopeySeconds' />
               <div className='card-img-overlay'>
                 {this.state.isMouseInsideProj4 ? <a className='projBtn' href='http://www.slopeyseconds.com/' target="_blank" rel="noopener noreferrer">Website ➜</a> : null}
