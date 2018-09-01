d26a6d2b22535e3450832628ecbc40fc1d19c245
kaconant <krissyconant@gmail.com>
2018-09-01 15:25:06 -0400

    more edits

--- src/About.js
+++ src/About.js
@@ -10,9 +10,9 @@ class About extends Component {
             <img className='krissyConant' src='/img/krissy.png' alt='krissy'/>
             <button className='resumeBtn'><a href="/img/KrissyConantResume.pdf" download="KrissyConantResume.pdf">Download My Resume ➜</a></button>
             <div className='allContact'>
-              <h3 className="resumeTitle">email me:<a href="mailto:krissyconant@gmail.com">hello@krissyconant.com</a></h3>
-              <h3 className="resumeTitle">linkedin:<a href="https://www.linkedin.com/in/krissy-conant/"> krissy-conant</a></h3>
-              <h3 className="resumeTitle">github:<a href="https://github.com/kaconant"> kaconant</a></h3>
+              <h3 className="resumeTitle">email me: <br/><a href="mailto:krissyconant@gmail.com">hello@krissyconant.com</a></h3>
+              <h3 className="resumeTitle">linkedin: <br/><a href="https://www.linkedin.com/in/krissy-conant/"> krissy-conant</a></h3>
+              <h3 className="resumeTitle">github: <br/><a href="https://github.com/kaconant"> kaconant</a></h3>
             </div>
           </div>
         </div>
@@ -24,7 +24,7 @@ class About extends Component {
           </div>
           <div className='profile'>
             <p className='profileTitle'>PRESENT</p>
-            <p className='profileBio'>By day: I'm a marketing specialist for a SaaS company where I wear many hats: content blogging, internal employee engagement and retention efforts, as well as building our company's industry connections via event and trade show planning and execution. <br/> <br/> By night: I've just completed a 26 week full stack web development course from DigitalCrafts where we've covered the 101 on how to be a developer in UI, resource, and devOps. 
+            <p className='profileBio'>By day: I'm a marketing specialist for a SaaS company where I wear many hats: content blogging, internal employee engagement and retention efforts, as well as building our company's industry connections via event and trade show planning and execution. <br/> <br/> By night: I've just completed a 26 week full stack web development course from DigitalCrafts where we've covered the 101 on how to be a developer in front-end, back-end, and dev-ops technologies. 
             </p>
           </div>
           <div className='profile'>
